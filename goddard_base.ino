c47e73cb7bc0e2b98345dac05241833176fb9844
William Stevens <contact@wastevensv.com>
2018-01-28 03:03:01 -0500

    Complete rewrite with new library. Works.

--- goddard_base.ino
+++ goddard_base.ino
@@ -1,62 +1,55 @@
 // -*- mode: C++ -*-
-// Example sketch showing how to create a simple messageing server
-// with the RH_NRF24 class. RH_NRF24 class does not provide for addressing or
-// reliability, so you should only use RH_NRF24  if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example nrf24_client
-// Tested on Uno with Sparkfun NRF25L01 module
-// Tested on Anarduino Mini (http://www.anarduino.com/mini/) with RFM73 module
-// Tested on Arduino Mega with Sparkfun WRL-00691 NRF25L01 module
-
 #include <SPI.h>
-#include <RH_NRF24.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 
-// Singleton instance of the radio driver
-RH_NRF24 nrf24(8, 10);
+//This pin is used to set the nRF24 to standby (0) or active mode (1)
+#define pinCE 8
 
-void setup()
+//This pin is used to tell the nRF24 whether the SPI communication
+// is a command or message to send out
+#define pinCSN 10
+uint8_t frame[] = "B090H"; // Packet format B[R][G][B][Emote].
+// Create your nRF24 object or wireless SPI connection
+RF24 wirelessSPI(pinCE, pinCSN);
+// Radio pipe addresses for the 2 nodes to communicate.
+const uint64_t pAddress = 0xB00B1E5000LL;
+
+void setup()  
 {
+  //start serial to communicate process
   Serial.begin(9600);
-  while (!Serial) 
-    ; // wait for serial port to connect. Needed for Leonardo only
-  if (!nrf24.init()) {
-    Serial.println("init failed");
-  } else {
-    Serial.println("init good");
-  }
-  // Defaults after init are 2.402 GHz (channel 2), 2Mbps, 0dBm
-  if (!nrf24.setChannel(1)) {
-    Serial.println("setChannel failed");
-  } else {
-    Serial.println("setChannel good");
-  }
-  if (!nrf24.setRF(RH_NRF24::DataRate250kbps, RH_NRF24::TransmitPower0dBm)) {
-    Serial.println("setRF failed"); 
-  } else {
-    Serial.println("setRF good");
-  }
+  //Start the nRF24 module
+  wirelessSPI.begin();
+  // Ensure autoACK is enabled so rec sends ack packet
+  // to let you know it got the transmit packet payload
+  wirelessSPI.setAutoAck(1);
+  // Allow optional ack payloads
+  wirelessSPI.enableAckPayload();
+  wirelessSPI.setPALevel(RF24_PA_MAX);
+  wirelessSPI.setDataRate(RF24_250KBPS);
+  // pipe address that we will communicate over,
+  // must be the same for each nRF24 module
+  wirelessSPI.openWritingPipe(pAddress);
+  //transmitter so stop listening for data
+  wirelessSPI.stopListening();
+  //use random ADC value to seed random number algorithm
+  randomSeed(analogRead(0));
 }
 
-void loop()
-{
-  if (nrf24.available())
-  {
-    uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
-    uint8_t len = sizeof(buf);
-    if (nrf24.recv(buf, &len))
-    {
-      Serial.print((char*)buf);
-    }
-    else
-    {
-      Serial.print('!');
-    }
-  }
-  if (Serial.avialable())
-  {
+void loop() {
+  delay(500);
+  while(Serial.available() > 0) {
       uint8_t inByte = Serial.read();
-      nrf24.send(inByte, 1);
-      nrf24.waitPacketSent();
+      if(inByte == 'B') {
+          Serial.readBytes(&frame[1],4);
+          //if the send fails let the user know over serial monitor
+          Serial.write((char*)frame);
+          if (wirelessSPI.write( frame, 5 )){
+              Serial.write(".");
+          } else {
+              Serial.write("!");
+          }
+      }
   }
 }
-
