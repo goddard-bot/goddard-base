3279435144081ebc8aa21a9fbe9d37f61cdfc72b
William Stevens <contact@wastevensv.com>
2018-01-27 23:22:00 -0500

    Possibly working relay code.

--- barn_base.ino
+++ barn_base.ino
@@ -14,9 +14,7 @@
 // Singleton instance of the radio driver
 RH_NRF24 nrf24(8, 10);
 
-     // RH_NRF24 nrf24(8, 7); // use this to be electrically compatible with Mirf
-// RH_NRF24 nrf24(8, 10);// For Leonardo, need explicit SS pin
-// RH_NRF24 nrf24(8, 7); // For RFM73 on Anarduino Mini
+void setup()
 {
   Serial.begin(9600);
   while (!Serial) 
@@ -32,7 +30,7 @@ RH_NRF24 nrf24(8, 10);
   } else {
     Serial.println("setChannel good");
   }
-  if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm)) {
+  if (!nrf24.setRF(RH_NRF24::DataRate250kbps, RH_NRF24::TransmitPower0dBm)) {
     Serial.println("setRF failed"); 
   } else {
     Serial.println("setRF good");
@@ -41,34 +39,24 @@ RH_NRF24 nrf24(8, 10);
 
 void loop()
 {
-  if (Serial.avialable())
-  {
-    uint8_t inByte = Serial.read()
-    switch(inByte)
-    {
-    }
-  }
   if (nrf24.available())
   {
-    // Should be a message for us now   
     uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
     if (nrf24.recv(buf, &len))
     {
-//      NRF24::printBuffer("request: ", buf, len);
-      Serial.print("got request: ");
-      Serial.println((char*)buf);
-      
-      // Send a reply
-      uint8_t data[] = "And hello back to you";
-      nrf24.send(data, sizeof(data));
-      nrf24.waitPacketSent();
-      Serial.println("Sent a reply");
+      Serial.print((char*)buf);
     }
     else
     {
-      Serial.println("recv failed");
+      Serial.print('!');
     }
+  }
+  if (Serial.avialable())
+  {
+      uint8_t inByte = Serial.read();
+      nrf24.send(inByte, 1);
+      nrf24.waitPacketSent();
   }
 }
 
